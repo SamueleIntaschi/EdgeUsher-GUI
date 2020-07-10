6926f032d13a0804946edcf3ad1570902ff35c27
SamueleIntaschi <s.intaschi2@studenti.unipi.it>
2020-07-10 17:41:10 +0200

    Deleting temporary files

--- EdgeUsher-backend/chain.pl
+++
@@ -1,6 +0,0 @@
-chain(catena_prova, [iot_controller, processing]).
-% Services
-service(iot_controller, 5, 2, [camera, bike], or(anti_tampering, encrypted_storage)).
-service(processing, 10, 2, [], []).
-% Links
-flow(iot_controller, processing, 8).
--- EdgeUsher-backend/infra.pl
+++
@@ -1,4 +0,0 @@
-0.5::node(cloud, 50, [camera, bike], [encrypted_storage, anti_tampering]);0.2::node(cloud, 1, [], []).
-node(isp, 1, [bike, camera], [encrypted_storage, anti_tampering]).
-0.5::link(cloud, isp, 8, 16);0.5::link(cloud, isp, 16, 9).
-link(isp, cloud, 12, 12).
